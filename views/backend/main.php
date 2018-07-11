515540ee08aa826a48790ed4f34d840429c48cf7
Trần Cảnh Dinh <40481356+dinhdeveloper@users.noreply.github.com>
2018-07-12 01:55:43 +0700

    Add files via upload
    update

--- views/backend/main.php
+++ views/backend/main.php
@@ -1,103 +1,114 @@
 <?php
-    /**
-     * Created by PhpStorm.
-     * User: Dinht
-     * Date: 6/22/2018
-     * Time: 1:07 AM
-	 */
-	session_start();
-	include '../../libs/database.php';
-	include 'dangnhap/kiemtradangnhap.php';
-	check_login();
-	// session_destroy();
+/**
+ * Created by PhpStorm.
+ * User: Dinht
+ * Date: 6/22/2018
+ * Time: 1:07 AM
+ */
+session_start();
+include '../../libs/database.php';
+include 'Admin/kiemtradangnhap.php';
+check_login();
+//định nghĩa biến cho từng trang:
+$c = 1;
+
+if (isset($_GET['c'])) {
+    $c = $_GET['c'];
+}
+// session_destroy();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="msapplication-tap-highlight" content="no">
-	<meta name="description"
-	      content="Materialize is a Material Design Admin Template,It's modern, responsive and based on Material Design by Google. ">
-	<meta name="keywords"
-	      content="materialize, admin template, dashboard template, flat admin template, responsive admin template,">
-	<title>Trang Quản Trị</title>
-	<!-- Favicons-->
-	<link rel="icon" href="../../images/favicon/favicon-32x32.png" sizes="32x32">
-	<!-- Favicons-->
-	<link rel="apple-touch-icon-precomposed" href="../../images/favicon/apple-touch-icon-152x152.png">
-	<!-- For iPhone -->
-	<meta name="msapplication-TileColor" content="#00bcd4">
-	<meta name="msapplication-TileImage" content="images/favicon/mstile-144x144.png">
-	<!-- For Windows Phone -->
-	<!-- CORE CSS-->
-	<link href="../../css/themes/fixed-menu/materialize.css" type="text/css" rel="stylesheet">
-	<link href="../../css/themes/fixed-menu/style.css" type="text/css" rel="stylesheet">
-	<!-- Custome CSS-->
-	<link href="../../css/custom/custom.css" type="text/css" rel="stylesheet">
-	<!-- INCLUDED PLUGIN CSS ON THIS PAGE -->
-	<link href="../../vendors/perfect-scrollbar/perfect-scrollbar.css" type="text/css" rel="stylesheet">
-	<link href="../../vendors/jvectormap/jquery-jvectormap.css" type="text/css" rel="stylesheet">
-	<link href="../../vendors/flag-icon/css/flag-icon.min.css" type="text/css" rel="stylesheet">
-	<link rel="stylesheet" href="https://code.jquery.com/jquery-3.3.1.min.js">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="msapplication-tap-highlight" content="no">
+    <meta name="description"
+          content="Materialize is a Material Design Admin Template,It's modern, responsive and based on Material Design by Google. ">
+    <meta name="keywords"
+          content="materialize, admin template, dashboard template, flat admin template, responsive admin template,">
+    <title>Trang Quản Trị</title>
+    <!-- Favicons-->
+    <link rel="icon" href="../../images/favicon/favicon-32x32.png" sizes="32x32">
+    <!-- Favicons-->
+    <link rel="apple-touch-icon-precomposed" href="../../images/favicon/apple-touch-icon-152x152.png">
+    <!-- For iPhone -->
+    <meta name="msapplication-TileColor" content="#00bcd4">
+    <meta name="msapplication-TileImage" content="images/favicon/mstile-144x144.png">
+    <!-- For Windows Phone -->
+    <!-- CORE CSS-->
+    <link href="../../css/themes/fixed-menu/materialize.css" type="text/css" rel="stylesheet">
+    <link href="../../css/themes/fixed-menu/style.css" type="text/css" rel="stylesheet">
+    <!-- Custome CSS-->
+    <link href="../../css/custom/custom.css" type="text/css" rel="stylesheet">
+    <!-- INCLUDED PLUGIN CSS ON THIS PAGE -->
+    <link href="../../vendors/perfect-scrollbar/perfect-scrollbar.css" type="text/css" rel="stylesheet">
+    <link href="../../vendors/jvectormap/jquery-jvectormap.css" type="text/css" rel="stylesheet">
+    <link href="../../vendors/flag-icon/css/flag-icon.min.css" type="text/css" rel="stylesheet">
+    <link rel="stylesheet" href="https://code.jquery.com/jquery-3.3.1.min.js">
 </head>
 <body>
 <?php
-	include_once 'modules/header.php';
-	include_once 'modules/menu_left.php';
-	//include_once 'modules/menu_content.php';
+include_once 'modules/header.php';
+include_once 'modules/menu_left.php';
+//include_once 'modules/menu_content.php';
 ?>
 <div id="content">
     <?php
-        //định nghĩa biến cho từng trang:
-        $c = 1;
-        
-        if(isset($_GET['c'])){
-            $c = $_GET['c'];
-        }
-        
-        switch($c){
-            case 1:
-                //include("modules/menu_content.php");
-                break;
-            case 2:
-                include("pages/taikhoan/pDanhsachtaikhoan.php"); //trang danh sách tài khoản nhân viên
-                break;
-            case 3:
-                include("pages/loaisanpham/pDanhsachloaisanpham.php");//trang danh sách loại sản phẩm
-                break;
-			case 4:
-                include("pages/sanpham/pDanhsachtatcasanpham.php"); //trang danh sách tất cả sản phẩm
-                break;
-			case 5:
-				include ("pages/loaisanpham/pPhanloaisanpham.php");//trang phân loại sản phẩm
-				break;
-			case 6:
-				include ("pages/sanpham/pTaosanphammoi.php"); //trang tạo sản phẩm mới
-				break;
-			case 7:
-				include ("pages/loaisanpham/pTaoloaisanpham.php"); //trang tạo loại sản phẩm mới
-				break;
-            case 8:
-                include ("pages/taikhoan/pCapnhattaikhoan.php");// trang cập nhật tài khoản
-                break;
-            case 9:
-                include ("pages/taikhoan/pThongtintaikhoan.php"); //trang thông tin tài khoản
-                break;
-            case 10:
-                include ("pages/loaisanpham/pCapnhatloaisanpham.php");//trang cập nhật loai sản phẩm
-                break;
-            case 11:
-                include ("pages/dondathang/danhsachdondathang.php");
-                break;
-            case 12:
-                include ("pages/dondathang/chitietdondathang.php");
-                break;
-            default:
-                include("pages/pError.php");
-        }
+
+    if (isset($_SESSION["SMaNhanVien"]) == false) {
+        $c = 0;
+    }
+    switch ($c) {
+        case 0:
+            include("Error/pError.php");
+            break;
+        case 1:
+            //include("modules/menu_content.php");
+            break;
+        //tài khoản
+        case 102:
+            include("pages/taikhoan/pThongtintaikhoan.php"); //trang thông tin tài khoản
+            break;
+        case 103:
+            include("pages/taikhoan/pCapnhattaikhoan.php");// trang cập nhật tài khoản
+            break;
+        case 104:
+            include("pages/taikhoan/pDanhsachtaikhoan.php"); //trang danh sách tài khoản nhân viên
+            break;
+        //Loại sản phẩm
+        case 201:
+            include("pages/loaisanpham/pDanhsachloaisanpham.php");//trang danh sách loại sản phẩm
+            break;
+        case 202:
+            include("pages/loaisanpham/pTaoloaisanpham.php"); //trang tạo loại sản phẩm mới
+            break;
+        case 203:
+            include("pages/loaisanpham/pCapnhatloaisanpham.php");//trang cập nhật loai sản phẩm
+            break;
+        case 204:
+            include("pages/loaisanpham/pPhanloaisanpham.php");//trang phân loại sản phẩm
+            break;
+        //Sản phẩm:
+        case 301:
+            include("pages/sanpham/pDanhsachtatcasanpham.php"); //trang danh sách tất cả sản phẩm
+            break;
+        case 302:
+            include("pages/sanpham/pTaosanphammoi.php"); //trang tạo sản phẩm mới
+            break;
+        //đơn đặt hàng
+        case 401:
+            include("pages/dondathang/danhsachdondathang.php");
+            break;
+        case 402:
+            include("pages/dondathang/chitietdondathang.php");
+            break;
+        default:
+            include("Error/pError.php");
+    }
+
     ?>
 </div>
 <!-- jQuery Library -->
