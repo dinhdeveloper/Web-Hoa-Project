46710cacfc22d93ffc1f9e80da99c35da4d93eb2
dinhdeveloper <dinhtrancntt@gmail.com>
2018-07-12 01:50:51 +0700

    update

--- views/backend/Error.php
+++
@@ -1,9 +0,0 @@
-<?php
-    /**
-     * Created by PhpStorm.
-     * User: Dinht
-     * Date: 6/30/2018
-     * Time: 4:05 PM
-     */
-    echo 'sai sai';
-    ?>
---
+++ views/backend/Error/pErrorAdmin.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DINHIT
+ * Date: 7/12/2018
+ * Time: 1:04 AM
+ */
